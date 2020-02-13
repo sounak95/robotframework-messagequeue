33ace6eea5c268e19cba05dc9434b21b2b0d8133
Saha,Sounak <Sounak.Saha@misys.com>
2020-02-13 17:46:31 +0530

    updated

---
+++ build/lib/MessageQueue/__init__.py
@@ -0,0 +1,1 @@
+from .MessageQueue import MessageQueue
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='MessageQueue',description='MessageQueue',
+long_description = 'MessageQueue',
+version = '1.0',
+url = '',
+author = '',
+author_email = '',
+license = 'Apache2',
+classifiers = [
+   'Development Status :: Version 1.1',
+   'Intended Audience :: System Administrators',
+   'License :: OSI Approved :: Apache Software License',
+   'Programming Language :: Python :: 3.7'
+],
+packages = ['MessageQueue'],
+install_requires = []
+)
